32117526d1005064ef4ae91d62392fedba2d3e24
Tushar <shendgetushar11@gmail.com>
2023-03-20 13:11:27 +0530

    cardCarouselNewAlbums-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Card from "./components/Card/Card";
+// import Card from "./components/Card/Card";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
-import { fetchTopSongs } from "./api/api";
+import { fetchTopAlbums, fetchNewAlbums } from "./api/api";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Section title="Top Albums" dataSource={fetchTopSongs} />
+      <Section title="Top Albums" dataSource={fetchTopAlbums} />
+      <Section title="New Albums" dataSource={fetchNewAlbums} />
     </>
   );
 }
--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -5,9 +5,17 @@ import axios from "axios";
 
 const BACKEND_URL = "https://qtify-backend-labs.crio.do";
 
-export const fetchTopSongs = async () => {
+export const fetchTopAlbums = async () => {
   try {
     const response = await axios.get(`${BACKEND_URL}/albums/top`);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const fetchNewAlbums = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/albums/new`);
     return response.data;
   } catch (e) {
     console.log(e);
